6c47dd67d4c15589e2b204b344bb02797750c3c3
NeroYuki <kingrfminecraft@gmail.com>
2018-07-04 23:04:42 +0700

    Add files via upload

---
+++ cmd/addtrack.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+
+module.exports.run = (client, message, args) => {
+		let uid = args[0];
+		if (isNaN(parseInt(uid))) {message.channel.send("Your uid please!")}
+		else {
+		fs.readFile("tracking.txt", 'utf8', function(err, data) {
+			if (err) throw err;
+			var dup=false;
+			var updatedata="";
+			let b = data.split('\n');
+			for (i=0;i<b.length;i++) {
+				if (b[i].includes(uid)) {
+					b[i]=b[i].replace(uid);
+                    dup=true;
+                    message.channel.send("this uid has been already added");
+                }
+			}
+			if (!dup) {
+				b.push(uid);
+			}
+			for (i=0;i<b.length;i++) {
+				updatedata = updatedata + b[i];
+				if (i!=b.length-1) {updatedata=updatedata+"\n"}
+			}
+			fs.writeFile("tracking.txt", updatedata, function(err) {
+				if (err) throw err;
+				message.channel.send("Now tracking uid "+uid);
+				});
+			});
+		}
+}
+
+module.exports.help = {
+	name: "addtrack"
+}
