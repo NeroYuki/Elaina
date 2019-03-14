28127b1e50080609bc9c4eac4b1b988ccf0ce56c
NeroYuki <kingrfminecraft@gmail.com>
2019-03-14 23:19:22 +0700

    Update recent5.js

--- cmd/recent5.js
+++ cmd/recent5.js
@@ -4,7 +4,6 @@ var droidapikey = process.env.DROID_API_KEY;
 
 function modread(input) {
 	var res = '';
-	if (!res) return res;
 	if (input.includes('n')) res += 'NF'
 	if (input.includes('h')) res += 'HD'
 	if (input.includes('r')) res += 'HR'
