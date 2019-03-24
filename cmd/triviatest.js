9b5f1fbb3ba3e56094dfaf37de4358dc21eac036
NeroYuki <kingrfminecraft@gmail.com>
2019-03-24 13:16:21 +0700

    Add files via upload

--- cmd/triviatest.js
+++ cmd/triviatest.js
@@ -17,7 +17,7 @@ function categoryRandomSelect() {
         total += config.question_weight[i];
     }
     var number = Math.floor(Math.random()*total);
-    //console.log(number);
+    console.log(number);
     var current = 0;
     for (i in config.question_weight) {
         current += config.question_weight[i];
@@ -41,28 +41,33 @@ module.exports.run = (client, message, args) => {
     switch (category) {
         case 0: question_dir = 'trivia/animal.txt'; category_name = 'Animals'; break;    
         case 1: question_dir = 'trivia/animemanga.txt'; category_name = 'Entertainment: Japanese Anime & Manga'; break; 
-        case 2: question_dir = 'trivia/art.txt'; category_name = 'Art'; break;
+        case 2: question_dir = 'trivia/artnliterature.txt'; category_name = 'Art and Literature'; break;
         case 3: question_dir = 'trivia/boardgame.txt'; category_name = 'Entertainment: Board Games'; break;
         case 4: question_dir = 'trivia/cartoon.txt'; category_name = 'Entertainment: Cartoon & Animations'; break;  
-        case 5: question_dir = 'trivia/book.txt'; category_name = 'Entertainment: Books'; break; 
-        case 6: question_dir = 'trivia/celeb.txt'; category_name = 'Celebrities'; break;  
-        case 7: question_dir = 'trivia/comic.txt'; category_name = 'Entertainment: Comics'; break;  
-        case 8: question_dir = 'trivia/computer.txt'; category_name = 'Science: Computers'; break; 
-        case 9: question_dir = 'trivia/film.txt'; category_name = 'Entertainment: Film'; break; 
-        case 10: question_dir = 'trivia/gadget.txt'; category_name = 'Science: Gadgets'; break;  
-        case 11: question_dir = 'trivia/general.txt'; category_name = 'General Knowledge'; break; 
-        case 12: question_dir = 'trivia/geography.txt'; category_name = 'Geography'; break;  
-        case 13: question_dir = 'trivia/history.txt'; category_name = 'History'; break;
-        case 14: question_dir = 'trivia/math.txt'; category_name = 'Science: Mathematics'; break;  
-        case 15: question_dir = 'trivia/music.txt'; category_name = 'Entertainment: Music'; break; 
-        case 16: question_dir = 'trivia/myth.txt'; category_name = 'Mythology'; break;  
-        case 17: question_dir = 'trivia/science.txt'; category_name = 'Science & Nature'; break;
-        case 18: question_dir = 'trivia/sport.txt'; category_name = 'Sports'; break;   
-        case 19: question_dir = 'trivia/television.txt'; category_name = 'Entertainment: Television'; break; 
-        case 20: question_dir = 'trivia/test.txt'; category_name = 'Test'; break;
-        case 21: question_dir = 'trivia/theater.txt'; category_name = 'Entertainment: Musicals & Theatres'; break;  
-        case 22: question_dir = 'trivia/vehicle.txt'; category_name = 'Vehicles'; break;  
-        case 23: question_dir = 'trivia/videogame.txt'; category_name = 'Entertainment: Video Games'; break; 
+        case 5: question_dir = 'trivia/celeb.txt'; category_name = 'Celebrities'; break;  
+        case 6: question_dir = 'trivia/comic.txt'; category_name = 'Entertainment: Comics'; break;  
+        case 7: question_dir = 'trivia/dota2.txt'; category_name = 'Video Game: DotA 2'; break;  
+        case 8: question_dir = 'trivia/ff.txt'; category_name = 'Video Game: Final Fantasy'; break;  
+        case 9: question_dir = 'trivia/computer.txt'; category_name = 'Science: Computers'; break; 
+        case 10: question_dir = 'trivia/film.txt'; category_name = 'Entertainment: Film'; break; 
+        case 11: question_dir = 'trivia/gadget.txt'; category_name = 'Science: Gadgets'; break;  
+        case 12: question_dir = 'trivia/general.txt'; category_name = 'General Knowledge'; break; 
+        case 13: question_dir = 'trivia/geography.txt'; category_name = 'Geography'; break;  
+        case 14: question_dir = 'trivia/history.txt'; category_name = 'History'; break;
+        case 15: question_dir = 'trivia/leagueoflegends.txt'; category_name = 'Video Game: League of Legends'; break; 
+        case 16: question_dir = 'trivia/math.txt'; category_name = 'Science: Mathematics'; break;  
+        case 17: question_dir = 'trivia/music.txt'; category_name = 'Entertainment: Music'; break; 
+        case 18: question_dir = 'trivia/myth.txt'; category_name = 'Mythology'; break;  
+        case 19: question_dir = 'trivia/pokemon.txt'; category_name = 'Video Game: Pokemon'; break; 
+        case 20: question_dir = 'trivia/science.txt'; category_name = 'Science & Nature'; break;
+        case 21: question_dir = 'trivia/slogan.txt'; category_name = 'Company Slogans'; break; 
+        case 22: question_dir = 'trivia/sport.txt'; category_name = 'Sports'; break;  
+        case 23: question_dir = 'trivia/starwars.txt'; category_name = 'Film: Star Wars'; break; 
+        case 24: question_dir = 'trivia/television.txt'; category_name = 'Entertainment: Television'; break; 
+        case 25: question_dir = 'trivia/test.txt'; category_name = 'Non-categorized'; break;
+        case 26: question_dir = 'trivia/theater.txt'; category_name = 'Entertainment: Musicals & Theatres'; break;  
+        case 27: question_dir = 'trivia/vehicle.txt'; category_name = 'Vehicles'; break;  
+        case 28: question_dir = 'trivia/videogame.txt'; category_name = 'Entertainment: Video Games'; break; 
         default: message.channel.send("Unknown type"); return;
     }
 	fs.readFile(question_dir , 'utf8', (err, data) => {
@@ -101,7 +106,7 @@ module.exports.run = (client, message, args) => {
             if (img_link && img_link != "-") embed["image"]["url"] = img_link;
             message.channel.send({embed});
             embed = {
-                "description": "The correct answer is " + String.fromCharCode(65+correct_index) + ". " + correct_answer,
+                "description": "**The correct answer is " + String.fromCharCode(65+correct_index) + ". " + correct_answer + "**",
                 "color": 8102199
             }
             setTimeout(() => {
@@ -111,7 +116,8 @@ module.exports.run = (client, message, args) => {
         }
 
         else if (type === 2) {
-            var correct_answer = component[4];
+            var correct_answer = [];
+            for (var i = 4; i < component.length; i++) correct_answer.push(component[i]);
             var embed = {
                 "description": difficulty + "★ - " + category_name + " Question" ,
                 "color": 8102199,
@@ -132,16 +138,16 @@ module.exports.run = (client, message, args) => {
             if (img_link && img_link != "-") embed["image"]["url"] = img_link;
             message.channel.send({embed});
             embed = {
-                "description": "The correct answer is " + correct_answer,
+                "description": "**The correct answer is " + correct_answer[0] + "**\n*Accepted answers are: " + correct_answer.join(", ") + "*",
                 "color": 8102199
             }
             setTimeout(() => {
-                answerCheck(correct_answer);
+                answerCheck(correct_answer[0], correct_answer);
                 message.channel.send({embed})
             }, 7000 + (difficulty * 3000))
         }
 
-        function answerCheck(answer) {
+        function answerCheck(answer, answer_array = []) {
             var correct_user = [];
             submitted_answer.shift();
             submitted_answer.sort((a, b) => {
@@ -150,9 +156,16 @@ module.exports.run = (client, message, args) => {
             for (var i = 1; i < submitted_answer.length; i++) {
                 if (submitted_answer[i][0] == submitted_answer[i-1][0]) {submitted_answer.splice(i-1, 1); i--;}
             }
-            for (var i = 0; i < submitted_answer.length; i++) {
-                if (submitted_answer[i][1].toLowerCase() == answer.toLowerCase()) correct_user.push(submitted_answer[i])
+            if (!answer_array[0]) {
+                for (var i = 0; i < submitted_answer.length; i++) 
+                    if (submitted_answer[i][1].toLowerCase() == answer.toLowerCase()) correct_user.push(submitted_answer[i])
             }
+            else {
+                for (var i = 0; i < submitted_answer.length; i++)
+                    for (var j = 0; j < answer_array.length; j++) 
+                        if (submitted_answer[i][1].toLowerCase() == answer_array[j].toLowerCase()) { correct_user.push(submitted_answer[i]); break; }
+            }
+            
             correct_user.sort((a, b) => {
                 return a[2] - b[2];
             })
