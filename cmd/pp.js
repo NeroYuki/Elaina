097ce8c1191036799f8d68c7005b141eee4b2a31
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-10-30 11:31:00 +0700

    Update pp.js

--- cmd/pp.js
+++ cmd/pp.js
@@ -139,10 +139,10 @@ function getMapPP(input, pcombo, pacc, pmissc, pmod = "", message, objcount, whi
 	})
 }
 
-module.exports.run = (client, message, args, maindb) => {
+module.exports.run = (client, message, args, maindb) => {message.delete();
 	let channel = message.guild.channels.find("name", "bot-ground");
 	let channel2 = message.guild.channels.find("name", "elaina-pp-project");
-	if (!channel && !channel2) {message.delete();
+	if (!channel && !channel2) {
 		message.channel.send(`${message.author}, ask server manager to create #elaina-pp-project or #bot-ground channel first!`).then (message => {message.delete(5000)});
 		return;
 	}
@@ -273,7 +273,7 @@ module.exports.run = (client, message, args, maindb) => {
 		}
 		else { message.channel.send("The account is not binded, you need to use `&userbind <uid>` first. To get uid, use `&profilesearch <username>`") };
 	});
-	} else {message.delete();
+	} else {
 		if (channel && channel2) {
 			message.channel.send(`${message.author}, that command is only allowed in ${channel} and ${channel2}!`).then(message => {
 				message.delete(5000)
