b869d078c8a17ae7d339faf6c08773a609e11938
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-08 23:22:31 +0700

    Update pp.js
    - PP submission rewrite (now use droid API instead of profile page under Edrows' request)
    - Remove unused function ranknum
    - Channel detection to prevent pp submission outside project channel and bot channel

--- cmd/pp.js
+++ cmd/pp.js
@@ -1,37 +1,20 @@
 var http = require('http');
-var mongodb = require('mongodb');
 var droid = require("./ojsamadroid");
 var https = require("https");
-var request = require("request")
+var request = require("request");
 require("dotenv").config();
 var apikey = process.env.OSU_API_KEY;
-
-function rankread(imgsrc) {
-	let rank="";
-	switch(imgsrc) {
-		case '<img src="assets/images/ranking-S-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-S-small.png";break;
-		case '<img src="assets/images/ranking-A-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-A-small.png";break;
-		case '<img src="assets/images/ranking-B-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-B-small.png";break;
-		case '<img src="assets/images/ranking-C-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-C-small.png";break;
-		case '<img src="assets/images/ranking-D-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-D-small.png";break;
-		case '<img src="assets/images/ranking-SH-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-SH-small.png";break;
-		case '<img src="assets/images/ranking-X-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-X-small.png";break;
-		case '<img src="assets/images/ranking-XH-small.png"/>':rank="http://ops.dgsrz.com/assets/images/ranking-XH-small.png";break;
-		default: rank="unknown";
-	}
-	return rank;
-}
-
+var droidapikey = process.env.DROID_API_KEY;
 
 function modenum(mod) {
 	var res = 4;
-	if (mod.includes("HardRock")) res += 16;
-	if (mod.includes("Hidden")) res += 8;
-	if (mod.includes("DoubleTime")) res += 64;
-	if (mod.includes("NightCore")) res += 576;
-	if (mod.includes("NoFail")) res += 1;
-	if (mod.includes("Easy")) res += 2;
-	if (mod.includes("HalfTime")) res += 256;
+	if (mod.includes("r")) res += 16;
+	if (mod.includes("h")) res += 8;
+	if (mod.includes("d")) res += 64;
+	if (mod.includes("c")) res += 576;
+	if (mod.includes("n")) res += 1;
+	if (mod.includes("e")) res += 2;
+	if (mod.includes("t")) res += 256;
 	return res;
 }
 
@@ -61,7 +44,7 @@ function getMapPP(input, pcombo, pacc, pmissc, pmod = "", message, objcount, whi
 				var obj = JSON.parse(content);
 				if (!obj[0]) {
 					console.log("Map not found"); 
-					message.channel.send("Error: The map you've played can't be found on osu! beatmap listing, please make sure the map is submitted, and up-to-date")
+					message.channel.send("Error: The map you've played can't be found on osu! beatmap listing, please make sure the map is submitted, and up-to-date");
 					objcount.x++;
 					return;
 				}
@@ -74,13 +57,13 @@ function getMapPP(input, pcombo, pacc, pmissc, pmod = "", message, objcount, whi
 					return;
 				}
 				//console.log(obj.beatmaps[0])
-				if (pmod) var mods = modenum(pmod)
+				if (pmod) var mods = modenum(pmod);
 				else var mods = 4;
-				if (pacc) var acc_percent = parseFloat(pacc)
+				if (pacc) var acc_percent = parseFloat(pacc);
 				else var acc_percent = 100;
-				if (pcombo) var combo = parseInt(pcombo)
+				if (pcombo) var combo = parseInt(pcombo);
 				else var combo;
-				if (pmissc) var nmiss = parseInt(pmissc)
+				if (pmissc) var nmiss = parseInt(pmissc);
 				else var nmiss = 0;
 				var parser = new droid.parser();
 				console.log(acc_percent);
@@ -95,7 +78,7 @@ function getMapPP(input, pcombo, pacc, pmissc, pmod = "", message, objcount, whi
 					// if (mods) {
 					// 	console.log("+" + osu.modbits.string(mods));
 					// }
-					if (pmod.includes("HardRock")) {
+					if (pmod.includes("r")) {
 						mods -= 16; 
 						cur_ar = Math.min(cur_ar*1.4, 10);
 						cur_od = Math.min(cur_od*1.4, 5);
@@ -123,9 +106,9 @@ function getMapPP(input, pcombo, pacc, pmissc, pmod = "", message, objcount, whi
 						acc_percent: acc_percent,
 					});
 					
-					parser.reset()
+					parser.reset();
 
-					if (pmod.includes("HardRock")) { mods += 16; }
+					if (pmod.includes("r")) { mods += 16; }
 					
 					console.log(nstars.toString());
 					console.log(npp.toString());
@@ -147,145 +130,143 @@ module.exports.run = (client, message, args, maindb) => {
 		return;
 	}
 	if (message.channel.name == 'bot-ground' || message.channel.name == 'elaina-pp-project') {
-	let objcount = {x: 0}
-	var offset = 1;
-	var start = 1;
-	if (args[0]) offset = parseInt(args[0]);
-	if (args[1]) start = parseInt(args[1]);
-	if (isNaN(offset)) offset = 1;
-	if (isNaN(start)) start = 1;
-	let ufind = message.author.id;
-	if (offset > 5 || offset < 1) offset = 1;
-	if (start + offset - 1 > 50) {console.log('out of limit'); return;}
-	// if (args[0]) {
+		let objcount = {x: 0};
+		var offset = 1;
+		var start = 1;
+		if (args[0]) offset = parseInt(args[0]);
+		if (args[1]) start = parseInt(args[1]);
+		if (isNaN(offset)) offset = 1;
+		if (isNaN(start)) start = 1;
+		let ufind = message.author.id;
+		if (offset > 5 || offset < 1) offset = 1;
+		if (start + offset - 1 > 50) {
+			console.log('Out of limit');
+			return;
+		}
+		// if (args[0]) {
 		// ufind = args[0];
 		// ufind = ufind.replace('<@!','');
 		// ufind = ufind.replace('<@','');
 		// ufind = ufind.replace('>','');
-	// }
-	console.log(ufind);
-	let binddb = maindb.collection("userbind");
-	let whitelist = maindb.collection("mapwhitelist");
-	let query = { discordid: ufind };
-	binddb.find(query).toArray(function(err, userres) {
-		if (err) throw err;
-		if (userres[0]) {
-			console.log(offset);
-			let uid = userres[0].uid;
-			let discordid = userres[0].discordid;
-			if (userres[0].pp) var pplist = userres[0].pp;
-			else var pplist = [];
-			if (userres[0].pptotal) var pre_pptotal = userres[0].pptotal;
-			else var pre_pptotal = 0
-			if (userres[0].playc) var playc = userres[0].playc;
-			else var playc = 0;
-			var pptotal = 0;
-			var submitted = 0;
-			var options = {
-				host: "ops.dgsrz.com",
-				port: 80,
-				path: "/profile.php?uid="+uid+".html"
-			};
+		// }
+		console.log(ufind);
+		let binddb = maindb.collection("userbind");
+		let whitelist = maindb.collection("mapwhitelist");
+		let query = {discordid: ufind};
+		binddb.find(query).toArray(function (err, userres) {
+			if (err) throw err;
+			if (userres[0]) {
+				console.log(offset);
+				console.log(userres[0]);
+				let uid = userres[0].uid;
+				let discordid = userres[0].discordid;
+				if (userres[0].pp) var pplist = userres[0].pp;
+				else var pplist = [];
+				if (userres[0].pptotal) var pre_pptotal = userres[0].pptotal;
+				else var pre_pptotal = 0;
+				if (userres[0].playc) var playc = userres[0].playc;
+				else var playc = 0;
+				var pptotal = 0;
+				var submitted = 0;
+				var options = {
+					host: "ops.dgsrz.com",
+					port: 80,
+					path: "/api/getuserinfo.php?apiKey=" + droidapikey + "&uid=" + uid
+				};
 
-			var content = "";   
+				var content = "";
 
-			var req = http.request(options, function(res) {
-				res.setEncoding("utf8");
-				res.on("data", function (chunk) {
-					content += chunk;
-				});
+				var req = http.request(options, function (res) {
+					res.setEncoding("utf8");
+					res.on("data", function (chunk) {
+						content += chunk;
+					});
 
-				res.on("end", function () {
-					const a = content;
-					let b = a.split('\n'), d = []; 
-					curpos = 0;
-					curentry = 0;
-					var playentry = [];
-					for (x = 0; x < b.length; x++) {
-						if (b[x].includes('<small>') && b[x - 1].includes('class="block"')) {
+					res.on("end", function () {
+						curpos = 0;
+						var playentry = [];
+						var resarr = content.split('<br>');
+						var obj = JSON.parse(resarr[1]);
+						var rplay = obj.recent;
+						for (var i = start - 1; i < start + offset - 1; i++) {
+							if (!rplay[i]) break;
 							var play = {
-								title: "", acc: "", miss: "", combo: "", mod: "", hash:""
-							}
-							b[x-1]=b[x-1].replace("<strong class=\"block\">","");
-							b[x-1]=b[x-1].replace("<\/strong>","");
-							b[x]=b[x].replace("<\/small>","");
-							b[x]=b[x].replace("<small>","");
-							b[x+1]=b[x+1].replace('<span id="statics" class="hidden">{"miss":','');
-							b[x+1]=b[x+1].replace('}</span>','')
-							play.title=b[x-1].trim();
-							var mshs = b[x+1].trim().split(',');
-							play.miss = mshs[0];
-							play.hash = mshs[1].split(':')[1];
-							//console.log(play.hash)
-							d = b[x].split("/"); ptime = d[0]; play.mod = d[2]; play.combo = d[3]; play.acc = d[4];
-							b[x-5]=b[x-5].trim();
+								title: "", acc: "", miss: "", combo: "", mod: "", hash: ""
+							};
+							play.title = rplay[i].filename;
+							play.acc = rplay[i].accuracy.toPrecision(4) / 1000;
+							play.miss = rplay[i].miss;
+							play.combo = rplay[i].combo;
+							play.mod = rplay[i].mode;
+							play.hash = rplay[i].hash;
+							playentry[curpos] = play;
 							curpos++;
-							if (curpos >= start) {playentry[curentry] = play; curentry++;}
-							if (curentry >= offset) break;
 						}
-						if (b[x].includes('h3 m-t-xs m-b-xs')) {
-							b[x]=b[x].replace('<div class="h3 m-t-xs m-b-xs">',"");
-							b[x]=b[x].replace('<\/div>',"");
-							b[x]=b[x].trim();
-							name = b[x]
-						}
-					}
-					
-					console.log(playentry);
-					playentry.forEach(function (x) {		
-						if (x.title) getMapPP (x.hash, x.combo, x.acc, x.miss, x.mod, message, objcount, whitelist, (pp, playinfo, hash, acc, combo, miss) => {
-							console.log(objcount);
-							var ppentry = [hash, playinfo, parseFloat(pp), acc, combo, miss]
-							var dup = false
-							//pplist.push(ppentry)
-							for (i in pplist) {
-								if (ppentry[0] == pplist[i][0]) {pplist[i] = ppentry; dup = true; playc++; break;} 
-							}
-							if (!dup) {pplist.push(ppentry); playc++;}
-							pplist.sort(function(a, b) {return b[2] - a[2]})
-							while (pplist.length > 75) pplist.pop();
-							submitted++;
-							if (objcount.x == playentry.length) {
-								var weight = 1;
+
+						console.log(playentry);
+						playentry.forEach(function (x) {
+							if (x.title) getMapPP(x.hash, x.combo, x.acc, x.miss, x.mod, message, objcount, whitelist, (pp, playinfo, hash, acc, combo, miss) => {
+								console.log(objcount);
+								var ppentry = [hash, playinfo, parseFloat(pp), acc, combo, miss];
+								var dup = false;
+								//pplist.push(ppentry)
 								for (i in pplist) {
-									pptotal += weight*pplist[i][2];
-									weight *= 0.95;
-								}
-								var diff = pptotal - pre_pptotal;
-								message.channel.send('<@' + discordid + '> Submitted ' + submitted + ' plays: + ' + diff.toFixed(2) + ' pp');
-								var updateVal = { $set: {
-										pptotal: pptotal,
-										pp: pplist,
-										playc: playc
+									if (ppentry[0] == pplist[i][0]) {
+										pplist[i] = ppentry;
+										dup = true;
+										playc++;
+										break;
 									}
 								}
-								binddb.updateOne(query, updateVal, function(err, res) {
-									if (err) throw err;
-									console.log('pp updated')
-									addcount = 0;
-								})
-							}
+								if (!dup) {
+									pplist.push(ppentry);
+									playc++;
+								}
+								pplist.sort(function (a, b) {
+									return b[2] - a[2]
+								});
+								while (pplist.length > 75) pplist.pop();
+								submitted++;
+								if (objcount.x == playentry.length) {
+									var weight = 1;
+									for (i in pplist) {
+										pptotal += weight * pplist[i][2];
+										weight *= 0.95;
+									}
+									var diff = pptotal - pre_pptotal;
+									message.channel.send('<@' + discordid + '> Submitted ' + submitted + ' plays: + ' + diff.toFixed(2) + ' pp');
+									var updateVal = {
+										$set: {
+											pptotal: pptotal,
+											pp: pplist,
+											playc: playc
+										}
+									};
+									binddb.updateOne(query, updateVal, function (err, res) {
+										if (err) throw err;
+										console.log('pp updated');
+										addcount = 0;
+									})
+								}
+							})
 						})
-					})
+					});
 				});
-			});
-			req.end();
-		}
-		else { message.channel.send("The account is not binded, you need to use `&userbind <uid>` first. To get uid, use `&profilesearch <username>`") };
-	});
-	} else {
-		message.author.lastMessage.delete();
+				req.end();
+			} else {
+				message.channel.send("The account is not binded, you need to use `&userbind <uid>` first. To get uid, use `&profilesearch <username>`")
+			};
+		});
+	} else {message.author.lastMessage.delete();
 		if (channel && channel2) {
 			message.channel.send(`${message.author}, that command is only allowed in ${channel} and ${channel2}!`).then(message => {
 				message.delete(5000)
 			});
-			return;
 		}
 		if (channel) {
 			message.channel.send(`${message.author}, that command is only allowed in ${channel}!`).then(message => {
 				message.delete(5000)
 			});
-			return;
 		}
 		if (channel2) {
 			message.channel.send(`${message.author}, that command is only allowed in ${channel2}!`).then(message => {
@@ -293,8 +274,8 @@ module.exports.run = (client, message, args, maindb) => {
 			});
 		}
 	}
-}
+};
 
 module.exports.help = {
 	name: "pp"
-}
+};
