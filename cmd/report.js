ddd5ae5dce8ce0d24d122cd8b631e31e160925ce
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-11-12 12:06:04 +0700

    Create report.js
    Report command based on [tempmute.js](https://github.com/NeroYuki/Elaina/blob/master/cmd/tempmute.js).
    
    Will need to add `"member cooldown"` (integer) and `"report_channel"` to [config.json](https://github.com/NeroYuki/Elaina/blob/master/config.json)
    
    Features:
    - Member role check to prevent reporting from DMs
    - Reply if no tagged user and reason
    - Sends report to `report_channel` and a copy of the report to the reporter
    - Cooldown system for 3 minutes (configurable in config file) for each member (Helpers and Moderators bypass this)

---
+++ cmd/report.js
@@ -0,0 +1,66 @@
+var Discord = require("discord.js");
+var config = require("../config.json");
+var cd = new Set();
+
+module.exports.run = async (client, message, args) => {
+    let user = message.author.id;
+    if (!message.channel instanceof Discord.DMChannel && !cd.has(user)) {
+        if (message.member.roles.find("name", "Helper") || message.member.roles.find("name", "Moderator")) cd.remove(user);
+        let toreport = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if (!toreport) return;
+        let reason = args.slice(1).join(" ");
+        if (!reason) {message.author.lastMessage.delete();
+            message.reply("please add a reason.");
+            return;
+        }
+        let channel = message.guild.channels.find(c => c.name === config.report_channel);
+        if (!channel) {
+            message.reply("please create a report log channel first!");
+            return;
+        }
+
+        await message.author.lastMessage.delete();
+
+        let reportembed = new Discord.RichEmbed()
+            .setDescription(`Report by ${message.author}`)
+            .setColor("#527ea3")
+            .setTimestamp(new Date())
+            .setFooter("React to this message upon completing report based on decision given")
+            .addField("Reported user: " + toreport.user.username, "Reported in: " + message.channel)
+            .addField("Reason: ", reason);
+
+        channel.send(reportembed);
+
+        let replyembed = new Discord.RichEmbed()
+            .setTitle("Report statistics")
+            .setColor("#527ea3")
+            .setTimestamp(new Date())
+            .setFooter("Elaina owo")
+            .addField("Reported user: " + toreport.user.username, "Reported in: " + message.channel)
+            .addField("Reason: " + reason, "Make sure you have evidence ready!\nAbuse of this command will result in a mute.");
+
+        try {
+            await message.author.send(replyembed);
+        } catch (e) {}
+
+        let cooldown = config.member_cooldown;
+        if (!message.member.roles.find("name", "Helper") && !message.member.roles.find("name", "Moderator")) {
+            cd.add(user);
+            setTimeout(() => {
+                cd.remove(user)
+            }, cooldown * 1000)
+        }
+    } else {
+        if (cd.has(message.author.id)) {
+            message.author.lastMessage.delete();
+            message.reply("you are still on cooldown!").then (message => {
+                message.delete(5000)
+            });
+        }
+        message.channel.send("This command is not allowed in DMs")
+    }
+};
+
+module.exports.help = {
+    name: "report"
+};
