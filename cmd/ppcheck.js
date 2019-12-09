3c7e6c314bb8f1ac93a41c8ad2b1f09a55e3e907
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2019-12-09 15:13:47 +0700

    Update ppcheck.js

--- cmd/ppcheck.js
+++ cmd/ppcheck.js
@@ -1,5 +1,37 @@
 var Discord = require('discord.js');
 var cd = new Set();
+let config = require('../config.json');
+
+function editpp(message, page, pp, ppentry, discordid, username, site, mirror) {
+	let embed = new Discord.RichEmbed()
+		.setDescription('**PP Profile for <@' + discordid + '> (' + username + ') [Page ' + page + '/15]**\nTotal PP: **' + pp + " pp**\n" + site + " - " + mirror)
+		.setColor(message.member.highestRole.hexColor)
+		.setFooter("Elaina owo", "https://images-ext-2.discordapp.net/external/d0iu_mPMvyoLQWnBSEnW4RL0-07KYm7zG9mjWdfWl7M/https/image.frl/p/yaa1nf94dho5f962.jpg");
+
+	for (var x = 5 * (page - 1); x < 5 + 5 * (page - 1); x++) {
+		if (ppentry[x]) {
+			let combo = ppentry[x][3];
+			if (!combo) combo = '0x';
+			combo = combo.toString();
+			if (combo.indexOf("x") == -1) combo = combo + "x";
+			else if (combo.indexOf(" ") != -1) combo = combo.replace(" ", "");
+
+			let acc = ppentry[x][4];
+			if (!acc) acc = '100.00%';
+			acc = acc.toString();
+			if (acc.indexOf('\r') != -1) acc = acc.replace(" ", "").replace("\r", "");
+			else if (acc.indexOf("%") != -1) acc = parseFloat(acc.trimRight()).toFixed(2) + "%";
+			else acc = acc + "%";
+
+			let miss = ppentry[x][5];
+			if (!miss) miss = '0 ❌';
+			else miss = miss.toString() + " ❌";
+			embed.addField((x+1) + '. ' + ppentry[x][1], combo + ' | ' + acc + " | " + miss + " | __" + ppentry[x][2] + ' pp__ (Net pp: ' + (ppentry[x][2] * Math.pow(0.95, x)).toFixed(2) + ' pp)')
+		}
+		else embed.addField((x+1) + '. -', '-')
+	}
+	return embed
+}
 
 module.exports.run = (client, message, args, maindb) => {
 	let ufind = message.author.id;
@@ -35,72 +67,33 @@ module.exports.run = (client, message, args, maindb) => {
 
 			let site = "[PP Profile](https://ppboard.herokuapp.com/profile?uid=" + uid + ")";
 			let mirror = "[Mirror](https://droidppboard.herokuapp.com/profile?uid=" + uid + ")";
-
-			let embed = new Discord.RichEmbed()
-				.setDescription('**PP Profile for <@' + discordid + '> (' + username + ') [Page ' + page + '/15]**\nTotal PP: **' + pp + " pp**\n" + site + " - " + mirror)
-				.setColor(message.member.highestRole.hexColor)
-				.setFooter("Elaina owo", "https://images-ext-2.discordapp.net/external/d0iu_mPMvyoLQWnBSEnW4RL0-07KYm7zG9mjWdfWl7M/https/image.frl/p/yaa1nf94dho5f962.jpg");
-
-			for (var x = 5 * (page - 1); x < 5 + 5 * (page - 1); x++) {
-				if (ppentry[x]) {
-					let combo = ppentry[x][3];
-					if (!combo) combo = '0x';
-					combo = combo.toString();
-					if (combo.indexOf("x") == -1) combo = combo + "x";
-					else if (combo.indexOf(" ") != -1) combo = combo.replace(" ", "");
-
-					let acc = ppentry[x][4];
-					if (!acc) acc = '100.00%';
-					acc = acc.toString();
-					if (acc.indexOf('\r') != -1) acc = acc.replace(" ", "").replace("\r", "");
-					else if (acc.indexOf("%") != -1) acc = parseFloat(acc.trimRight()).toFixed(2) + "%";
-					else acc = acc + "%";
-
-					let miss = ppentry[x][5];
-					if (!miss) miss = '0 ❌';
-					else miss = miss.toString() + " ❌";
-					embed.addField((x+1) + '. ' + ppentry[x][1], combo + ' | ' + acc + " | " + miss + " | __" + ppentry[x][2] + ' pp__ (Net pp: ' + (ppentry[x][2] * Math.pow(0.95, x)).toFixed(2) + ' pp)')
-				}
-				else embed.addField((x+1) + '. -', '-')
-			}
+			let embed = editpp(message, page, pp, ppentry, discordid, username, site, mirror);
 
 			message.channel.send(embed).then(msg => {
-				msg.react("⬅️").then(() => {
-					msg.react("➡️");
+				msg.react("⏮️").then(() => {
+					msg.react("⬅️").then(() => {
+						msg.react("➡️").then(() => {
+							msg.react("⏭️").catch(e => console.log(e))
+						})
+					})
 				});
+
+				let backward = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⏮️' && user.id === message.author.id, {time: 60000});
 				let back = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⬅️' && user.id === message.author.id, {time: 60000});
 				let next = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '➡️' && user.id === message.author.id, {time: 60000});
+				let forward = msg.createReactionCollector((reaction, user) => reaction.emoji.name === '⏭️' && user.id === message.author.id, {time: 60000});
+
+				backward.on('collect', () => {
+					page = 1;
+					embed = editpp(message, page, pp, ppentry, discordid, username, site, mirror);
+					msg.edit(embed).catch(e => console.log(e));
+					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
+				});
 
 				back.on('collect', () => {
 					if (page === 1) page = 15;
 					else page--;
-					embed = new Discord.RichEmbed()
-						.setDescription('**PP Profile for <@' + discordid + '> (' + username + ') [Page ' + page + '/15]**\nTotal PP: **' + pp + " pp**\n" + site + " - " + mirror)
-						.setColor(message.member.highestRole.hexColor)
-						.setFooter("Elaina owo", "https://images-ext-2.discordapp.net/external/d0iu_mPMvyoLQWnBSEnW4RL0-07KYm7zG9mjWdfWl7M/https/image.frl/p/yaa1nf94dho5f962.jpg");
-
-					for (var x = 5 * (page - 1); x < 5 + 5 * (page - 1); x++) {
-						if (ppentry[x]) {
-							let combo = ppentry[x][3];
-							if (!combo) combo = '0x';
-							combo = combo.toString();
-							if (combo.indexOf("x") == -1) combo = combo + "x";
-							else if (combo.indexOf(" ") != -1) combo = combo.replace(" ", "");
-
-							let acc = ppentry[x][4];
-							if (!acc) acc = '100.00%';
-							acc = acc.toString();
-							if (acc.indexOf('\r') != -1) acc = acc.replace(" ", "").replace("\r", "");
-							else if (acc.indexOf("%") != -1) acc = parseFloat(acc.trimRight()).toFixed(2) + "%";
-							else acc = acc + "%";
-
-							let miss = ppentry[x][5];
-							if (!miss) miss = '0 ❌';
-							else miss = miss.toString() + " ❌";
-							embed.addField((x+1) + '. ' + ppentry[x][1], combo + ' | ' + acc + " | " + miss + " | __" + ppentry[x][2] + ' pp__ (Net pp: ' + (ppentry[x][2] * Math.pow(0.95, x)).toFixed(2) + ' pp)')
-						}
-						else embed.addField((x+1) + '. -', '-')
-					}
+					embed = editpp(message, page, pp, ppentry, discordid, username, site, mirror);
 					msg.edit(embed).catch(e => console.log(e));
 					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
 				});
@@ -108,33 +101,14 @@ module.exports.run = (client, message, args, maindb) => {
 				next.on('collect', () => {
 					if (page === 15) page = 1;
 					else page++;
-					embed = new Discord.RichEmbed()
-						.setDescription('**PP Profile for <@' + discordid + '> (' + username + ') [Page ' + page + '/15]**\nTotal PP: **' + pp + " pp**\n" + site + " - " + mirror)
-						.setColor(message.member.highestRole.hexColor)
-						.setFooter("Elaina owo", "https://images-ext-2.discordapp.net/external/d0iu_mPMvyoLQWnBSEnW4RL0-07KYm7zG9mjWdfWl7M/https/image.frl/p/yaa1nf94dho5f962.jpg");
+					embed = editpp(message, page, pp, ppentry, discordid, username, site, mirror);
+					msg.edit(embed).catch(e => console.log(e));
+					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
+				});
 
-					for (var x = 5 * (page - 1); x < 5 + 5 * (page - 1); x++) {
-						if (ppentry[x]) {
-							let combo = ppentry[x][3];
-							if (!combo) combo = '0x';
-							combo = combo.toString();
-							if (combo.indexOf("x") == -1) combo = combo + "x";
-							else if (combo.indexOf(" ") != -1) combo = combo.replace(" ", "");
-
-							let acc = ppentry[x][4];
-							if (!acc) acc = '100.00%';
-							acc = acc.toString();
-							if (acc.indexOf('\r') != -1) acc = acc.replace(" ", "").replace("\r", "");
-							else if (acc.indexOf("%") != -1) acc = parseFloat(acc.trimRight()).toFixed(2) + "%";
-							else acc = acc + "%";
-
-							let miss = ppentry[x][5];
-							if (!miss) miss = '0 ❌';
-							else miss = miss.toString() + " ❌";
-							embed.addField((x+1) + '. ' + ppentry[x][1], combo + ' | ' + acc + " | " + miss + " | __" + ppentry[x][2] + ' pp__ (Net pp: ' + (ppentry[x][2] * Math.pow(0.95, x)).toFixed(2) + ' pp)')
-						}
-						else embed.addField((x+1) + '. -', '-')
-					}
+				forward.on('collect', () => {
+					page = 15;
+					embed = editpp(message, page, pp, ppentry, discordid, username, site, mirror);
 					msg.edit(embed).catch(e => console.log(e));
 					msg.reactions.forEach(reaction => reaction.remove(message.author.id).catch(e => console.log(e)))
 				})
@@ -145,7 +119,7 @@ module.exports.run = (client, message, args, maindb) => {
 			}, 10000)
 		}
 		else message.channel.send("The account is not binded, he/she/you need to use `&userbind <uid>` first. To get uid, use `&profilesearch <username>`")
-	});
+	})
 };
 
 module.exports.help = {
