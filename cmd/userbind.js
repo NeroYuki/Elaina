4d2cb7db365b4812e9240c42d2e13f613057afb7
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2020-01-26 16:12:22 +0700

    Prevent duplicate bind and fix Infinity bind

--- cmd/userbind.js
+++ cmd/userbind.js
@@ -3,7 +3,7 @@ const droidapikey = process.env.DROID_API_KEY;
 
 module.exports.run = (client, message, args, maindb) => {
 	let uid = args[0];
-	if (!uid) {message.channel.send("Your uid please!"); return;}
+	if (!uid) return message.channel.send("Your uid please!");
 	if (isNaN(uid)) {message.channel.send("Invalid uid")}
 	else {
 		let binddb = maindb.collection("userbind");
@@ -26,47 +26,54 @@ module.exports.run = (client, message, args, maindb) => {
 				return message.channel.send("Error: Unable to retrieve user data. Please try again!")
 			});
 			res.on("end", function () {
+				if (content.includes("<html>")) return message.channel.send("Invalid uid");
 				var headerres = content.split('<br>')[0].split(" ");
-				if (headerres[0] == 'FAILED') return message.channel.send("User not found!");
+				if (headerres[0] == 'FAILED') return message.channel.send("User doesn't exist");
 				let name = headerres[2];
-				var bind = {
-					discordid: message.author.id,
-					uid: uid,
-					username: name,
-					pptotal: 0,
-					pp: []
-				};
-				var updatebind = {
-					$set: {
+				let uid = headerres[1];
+				binddb.find({uid: uid}).toArray(function (err, res) {
+					if (err) throw err;
+					if (res[0]) return message.channel.send("Uid is already binded");
+					var bind = {
 						discordid: message.author.id,
 						uid: uid,
-						username: name
-					}
-				};
-				binddb.find(query).toArray(function (err, res) {
-					if (err) {
-						console.log(err);
-						return message.channel.send("Error: Empty database response. Please try again!")
-					}
-					if (!res[0]) {
-						binddb.insertOne(bind, function (err, res) {
-							if (err) {
-								console.log(err);
-								return message.channel.send("Error: Empty database response. Please try again!")
-							}
-							console.log("bind added");
-							message.channel.send("Haii <3, binded <@" + message.author.id + "> to uid " + uid);
-						})
-					} else {
-						binddb.updateOne(query, updatebind, function (err, res) {
-							if (err) {
-								console.log(err);
-								return message.channel.send("Error: Empty database response. Please try again!")
-							}
-							console.log("bind updated");
-							message.channel.send("Haii <3, binded <@" + message.author.id + "> to uid " + uid);
-						})
-					}
+						username: name,
+						pptotal: 0,
+						playc: 0,
+						pp: []
+					};
+					var updatebind = {
+						$set: {
+							discordid: message.author.id,
+							uid: uid,
+							username: name
+						}
+					};
+					binddb.find(query).toArray(function (err, res) {
+						if (err) {
+							console.log(err);
+							return message.channel.send("Error: Empty database response. Please try again!")
+						}
+						if (!res[0]) {
+							binddb.insertOne(bind, function (err, res) {
+								if (err) {
+									console.log(err);
+									return message.channel.send("Error: Empty database response. Please try again!")
+								}
+								console.log("bind added");
+								message.channel.send("Haii <3, binded <@" + message.author.id + "> to uid " + uid);
+							})
+						} else {
+							binddb.updateOne(query, updatebind, function (err, res) {
+								if (err) {
+									console.log(err);
+									return message.channel.send("Error: Empty database response. Please try again!")
+								}
+								console.log("bind updated");
+								message.channel.send("Haii <3, binded <@" + message.author.id + "> to uid " + uid);
+							})
+						}
+					})
 				})
 			})
 		});
